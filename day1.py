7f3c035e216a9f1b30e0b7af1899a3885ef0e74e
Eclipse_K <godo8522@naver.com>
2022-04-07 14:26:44 +0900

    first

--- day1.py
+++ day1.py
@@ -1,1 +1,16 @@
-pirnt(11*11)
+print(3*4)
+
+burger = 3000
+suger = 2000
+sort = 2500
+
+print(burger+suger+sort)
+
+def hello():
+    print("hello!")
+    print("Welcome to Codeit")
+
+hello()
+
+
+
